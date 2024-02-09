4d749b0ad10abfab1932c944144d39ef3b99c024
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-02-09 15:55:46 +0100

    fix: remove unused files

--- jsx-runtime.ts
+++
@@ -1,5 +0,0 @@
-const isReact = Deno.args[0] === "react";
-const { jsx, jsxs, Fragment, jsxDEV } = isReact
-  ? await import("islet/react/jsx-runtime")
-  : await import("islet/preact/jsx-runtime");
-export { jsx, jsxs, jsxDEV, Fragment };
