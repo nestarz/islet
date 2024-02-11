50e08a3733707fe47f413a5ea46d9aaffba80456
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-02-11 14:53:54 +0100

    Update jsx-runtime.ts

--- src/react/jsx-runtime.ts
+++ src/react/jsx-runtime.ts
@@ -1,19 +1,7 @@
 export * from "react/jsx-runtime";
 import { jsx as _jsx } from "react/jsx-runtime";
-import {
-  Children,
-  cloneElement,
-  createElement as h,
-  isValidElement,
-} from "react";
+import { cloneElement, Fragment } from "react";
 import { createJsx } from "../../server.ts";
 
-export const jsx = createJsx({
-  jsx: _jsx,
-  cloneElement,
-  h: _jsx,
-  toChildArray: Children.toArray,
-  isValidElement
-});
-
+export const jsx = createJsx({ cloneElement, h: _jsx, Fragment });
 export const jsxs = jsx;
